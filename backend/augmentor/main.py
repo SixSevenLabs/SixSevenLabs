7c34e6d72a5506b8ee981b0fbb04f6f9f82837db
akuwuh <isaacnguyen03@gmail.com>
2025-09-28 22:14:37 -0400

    started boilerplates

---
+++ backend/augmentor/main.py
@@ -0,0 +1,12 @@
+def main():
+    """
+    Main function of the script.
+    Contains the core logic to be executed when the script runs directly.
+    """
+    print("hello")
+    # Add your main program logic here
+
+if __name__ == "__main__":
+    # This block ensures that main() is called only when the script is executed directly.
+    # It will not run if the script is imported as a module into another script.
+    main()
